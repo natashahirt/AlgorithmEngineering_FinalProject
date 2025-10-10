4d63559bf788693194fb72121788c75a635eaf9b
Natasha Hirt <nhirt@mit.edu>
2025-10-10 12:11:26 -0400

    added testsuite to be run through pkg

---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+# runtests.jl is the standard julia interface for running tests
+# from the Pkg environment, run "test" and it will automatically
+# install all the required dependencies and then run the testsets
+
+include("test_admm_core.jl")
